5ba28a06df1b0ffb83d980681422da4ced88e29f
ivan-vukosavljevic@ymail.com <ivanvukosavljevic@ymail.com>
2020-08-05 21:41:31 +0200

    Unos 3 5.08.2020

--- cypress/integration/task.spec.js
+++ cypress/integration/task.spec.js
@@ -1,10 +1,17 @@
-const { EMAIL } = require("../fixtures/constants")
-import { createGalleryPage } from '../page_object/createGallery.page';
+import {EMAIL} from '../fixtures/constants';
+import {NAME} from '../fixtures/constants';
 import { authPage } from '../page_object/login.page';
-import { deletPage } from '../page_object/deleteGallery.page';
-import { eq } from 'cypress/types/lodash';
+import { randomEmail } from '../utils';
+import { createGalleryPage } from '../page_object/createGallery.page';
+
+
+const faker = require('faker');
+let email = faker.internet.email();
+let password = faker.internet.password();
+let url1 = "https://gallery-app.vivifyideas.com/";
 let img1 = "https://image.freepik.com/free-photo/black-white-angelfish-swimming-aqua-scape-planted-tropical-fish-tank_47840-73.jpg";
 let img2 = "https://thewallpaper.co//wp-content/uploads/2019/09//reptiles-lizard-cute-pc-tropical-pet-images-animals-samsung-green-color-jpg.jpg";
+
 
 
 describe('Task1', () => {
@@ -25,13 +32,20 @@ describe('Task1', () => {
     cy.wait(2000)
     cy.visit('/')
     cy.get('.container').contains('My Galleries').click()
+    cy.wait(1000)
     cy.get('.box-title').eq(0).click()
     cy.wait(1000)
     // cy.visit('/galleries/1607')
     cy.get('.btn').contains('Edit Gallery').click()
     cy.wait(1000)
-    cy.get('[type="button"]').contains('Add image').click()
-    cy.get('.fas').eq(0).click()
+    cy.get('button[type="button"]').contains('Add image').click()
+    cy.get('.fas').eq(2).click()
+    cy.get('.form-control').eq(2).type(img1)
+    cy.get('.btn').contains('Submit').click()
+    cy.visit('/my-galleries')
+    cy.get('.box-title').eq(0).click()
+    cy.wait(1000)
+    cy.get('.btn').contains('Delete Gallery').click()
   })
 
 })
